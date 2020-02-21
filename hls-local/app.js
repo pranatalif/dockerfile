b71256cf76379af2dabf9f3f026d424e0a48dbb3
pranatalif <alifakbarp@gmail.com>
2020-02-21 11:23:30 +0100

    add local testing

---
+++ hls-local/app.js
@@ -0,0 +1,6 @@
+var data = require('./keycloak1.json');
+//Manipulate data
+delete data['auth-server-url'];
+data['auth-server-url'] = 'http://' + process.env.KEYCLOAKIPRESOLVE + ':8080/auth';
+//Output data
+console.log(JSON.stringify(data));
